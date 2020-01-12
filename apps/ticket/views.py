3315daad6aecdca3c14bbfc5515b948e9a0fe37c
MjekuGresa <gresa.mjeku@outlook.com>
2020-01-12 16:33:24 +0100

    Changes in ticket app

--- apps/ticket/views.py
+++ apps/ticket/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect, HttpResponse
 from ..user.models import User
 from .models import Ticket
-from ..winning_numbers import winning_nums
+from ..winning_numbers import *
 import datetime
+from datetime import date, timedelta
+import ast
 
-now = datetime.datetime.now()
+def week_range():
+    week_start = date.today()
+    week_start -= timedelta(days=week_start.weekday())
+    week_end = week_start + timedelta(days=7)
+    return week_start, week_end
+
+def drawDay():
+    draw_day = False
+    now = datetime.datetime.now()
+    day, hour = now.weekday(), now.hour
+    if day == 6 and hour in range(10,15):
+        draw_day = True
+    return draw_day
 
 def index(request):
-    global now
-    day, hour, min = now.day, now.hour, now.min
-    if day == 4 and hour == 20 and min == 0: #draw_day = Friday
-        draw_day = True
-    else:
-        draw_day = False
     context = {
         'user': User.objects.get(id=request.session['user_id']), 
         'loop': range(1,40),
-        'draw_day': draw_day
+        'draw_day': drawDay()
     }
     if 'user_id' not in request.session:
         return redirect('user:index')
@@ -36,23 +44,30 @@ def new(request):
     Ticket.objects.create_ticket(request.POST.getlist('numbers_selected'), request.session['user_id'])
     return redirect('ticket:all')
 
-def notify(request):
-    global now
-    day, hour, min = now.day, now.hour, now.min
-    if day == 4 and hour == 20 and min == 0: #draw_day = Friday
-        draw_day = True
-        draw_numbers = winning_nums()
+def notify(request):  
+    week_start, week_end = week_range()
+    tickets_weekly_char = ""
+    tickets_weekly = []
+    for ticket in Ticket.objects.filter(created_at__range=(week_start,week_end)):
+        tickets_weekly_char += ticket.numbers_selected + ','
+    
+    #bad_chars = ["'"] 
+    #for i in bad_chars : 
+    #tickets_weekly_char = tickets_weekly_char.replace("'", '')
+    tickets_weekly = ast.literal_eval(tickets_weekly_char.replace("'",''))  
+    if drawDay():
+        #draw_numbers = winning_nums()
+        guessed_numbers = winning_ticket([1,3,5,7,9,11,13], tickets_weekly)
     else:
-        draw_day = False
-        draw_numbers = []
+        #draw_numbers = []
+        guessed_numbers = []
 
     context = {
         'user': User.objects.get(id=request.session['user_id']),
-        'tickets': Ticket.objects.order_by('-created_at'),
-        'draw_day':  draw_day,
-        'draw_numbers': draw_numbers,
-        'draw_numbers_compare': sorted(draw_numbers),
-        'result': False
+        'tickets_weekly': Ticket.objects.filter(created_at__range=(week_start,week_end)),
+        'draw_day':  drawDay(),
+        'draw_numbers': [1,3,5,7,9,11,13],
+        'guessed_numbers': guessed_numbers
     }
     if 'user_id' not in request.session:
         return redirect('user:index')
--- apps/winning_numbers.py
+++ apps/winning_numbers.py
@@ -8,3 +8,13 @@ def winning_nums():
             x = random.randint(1,40)
         l.append(x)
     return l
+
+def winning_ticket(draw_numbers, tickets):
+    list_count = []
+    for ticket in tickets:
+        count = 0
+        for i in range(0, len(ticket)):
+            if(int(ticket[i]) in draw_numbers):
+                count = count + 1
+        list_count.append(count)
+    return list_count
